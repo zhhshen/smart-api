ffc1bca2874ab74935a48c3650861a8ee207042a
shenzhonghui <shenzhonghui001@lianjia.com>
2019-05-08 20:04:58 +0800

    feat:init project

---
+++ README.md
@@ -0,0 +1,28 @@
+### Smart Api
+---
+
+#### 描述
+
+```
+根据项目中的mock数据，生成apiConfig文件，并自动替换指定文件中的api, 同时可以撤销相应的操作
+```
+
+#### 安装
+
+```bash
+npm install s-sapi -g
+```
+
+#### 配置项
+
+```
+命令：
+  sapi init [name]  init a config file
+  sapi replace      replace api
+  sapi reset        reset api
+
+选项：
+  --version, -V  显示版本号                                               [布尔]
+  --help, -h     显示帮助信息                                             [布尔]
+```
+
---
+++ bin/sapi.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+"use strict"
+
+const pkg = require('../package.json')
+const run = require("../lib")
+const chalk = require('chalk')
+
+const yargs = require('yargs')
+.usage(chalk.blue("sapi <init|update|clean>"))
+.version('version', chalk.blue(pkg.version))
+.alias('version', 'V')
+.command({
+  command: 'init',
+  alias: 'init',
+  desc: chalk.blue('init a config file'),
+  handler: argv => {
+    run.init()
+  }
+})
+.command({
+  command: 'replace',
+  alias: 'replace',
+  desc: chalk.blue('replace api'),
+  handler: argv => {
+    run.replace()
+  }
+})
+.command({
+  command: 'reset',
+  alias: 'reset',
+  desc: chalk.blue('reset api'),
+  handler: argv => {
+    run.reset()
+  }
+})
+.command({
+  command: 'clean',
+  alias: 'clean',
+  desc: chalk.blue('clean api'),
+  handler: argv => {
+    run.clean()
+  }
+})
+.demandCommand(1, 'must provide a valid command')
+.help('help')
+.alias('help', 'h')
+.epilogue('for more information, to be continue!')
+.argv
